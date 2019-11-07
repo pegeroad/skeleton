4512fc699079a3ce24a1baeede355cb0d94516c8
Gergo Petrik <petrikgergo@gmail.com>
2019-11-07 19:13:23 +0000

    mockito added

--- src/test/java/com/petrik/practice/skeleton/MainTest.java
+++ src/test/java/com/petrik/practice/skeleton/MainTest.java
@@ -3,7 +3,10 @@ package com.petrik.practice.skeleton;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class MainTest {
 
     private Main client;
